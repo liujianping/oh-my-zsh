d8de95bd636f01c17ca22a5764c205be883adf55
liujianping <liujianping.itech@qq.com>
2019-06-14 11:05:40 +0800

    1. support go cover val/cmp/web 2. fzf priview alias

--- plugins/alias/alias.plugin.zsh
+++ plugins/alias/alias.plugin.zsh
@@ -4,7 +4,13 @@ function alias-find(){
 
 alias af="alias-find "
 
-##kubectl alias
+# z jump ;;; use autojump
+# . /usr/local/etc/profile.d/z.sh
+
+# c alias for vscode
+alias c="code"
+
+##kubectl alias, kubectl installed required
 alias kls="kcgc"
 alias kcd="kcuc"
 
@@ -18,5 +24,49 @@ function reboot(){
 
 alias lk="lock"
 
-## golang code review
+## golang code review, golangci-lint installed required
 alias glt="golangci-lint"
+
+cover () {
+  t=$(mktemp)
+  go test $COVERFLAGS -coverprofile=$t $@ && go tool cover -func=$t && unlink $t
+}
+
+cover-val(){
+  cover $@ | grep ^total | awk '{print $3}' | awk -F. '{print $1}'
+}
+
+cover-cmp(){
+  cmp=$1
+  shift
+  val=$(cover-val $@)
+  num=$(($val+0))
+  if [ $cmp -gt  $num ]; then
+	echo "coverage insufficient."
+	return 1
+  fi	
+  return 0
+}
+
+cover-web() {
+  t=$(mktemp)
+  go test $COVERFLAGS -coverprofile=$t $@ && go tool cover -html=$t && unlink $t
+}
+
+## replace ls as exa ;;; brew install exa , required
+alias ls="exa"
+alias lt="lsa -T"
+
+## fzf alias
+ 
+function fzf-preview(){
+	fzf --preview '[[ $(file --mime {}) =~ binary ]] &&
+                 echo {} is a binary file ||
+                 (bat --style=numbers --color=always {} ||
+                  highlight -O ansi -l {} ||
+                  coderay {} ||
+                  rougify {} ||
+                  cat {}) 2> /dev/null | head -500'
+}
+
+alias fp="fzf-preview"
